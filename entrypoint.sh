1019ec94bf7796de686b87a51fcea5c790a2985d
Liam Gaffney <lpgaff@gmail.com>
2022-04-01 16:27:11 +0100

    Switch to EOS

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,8 @@
 # Run elog, for testing
 #elogd -p 9090 -c /etc/elogd.cfg -D
 
-# Run elog, for real
-elogd -p 8080 -c /elog-nfs/elogd.cfg
+# Run elog, for real (old style)
+#elogd -p 8080 -c /elog-nfs/elogd.cfg
+
+# Run elog, from EOS
+elogd -p 8080 -c /eos/home-i/isoelog/elog/elogd.cfg -t ${ELOGPWD}
