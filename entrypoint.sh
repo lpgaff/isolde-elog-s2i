5907725f154c729c187df3706500c872a7ffcb3e
Liam Gaffney <lpgaff@gmail.com>
2022-10-27 22:32:09 +0100

    switch back to local disk

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 #elogd -p 9090 -c /etc/elogd.cfg -D
 
 # Run elog, for real (old style)
-#elogd -p 8080 -c /elog-nfs/elogd.cfg
+elogd -p 8080 -c /elog-nfs/elogd.cfg
 
 # Run elog, from EOS
-elogd -p 8080 -c /eos/home-i/isoelog/elog/elogd.cfg
+#elogd -p 8080 -c /eos/home-i/isoelog/elog/elogd.cfg
