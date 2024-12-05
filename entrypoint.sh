9866342b90b7286487778705f6577b7ebbc74981
Liam Gaffney <lpgaff@gmail.com>
2024-12-05 23:28:49 +0000

    correct the e

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 #elogd -p 8080 -c /usr/local/elog/elogd.cfg
 
 # Run elog, for real (old style)
-elogd -p 8080 -c /elog-nfs/elogd.cfg
+elogd -p 8080 -c /elog-nfs-2/elogd.cfg
 
 # Run elog, from EOS
 #elogd -p 8080 -c /eos/home-i/isoelog/elog/elogd.cfg
