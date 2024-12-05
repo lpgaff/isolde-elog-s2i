d8594a27176c5f73620710292255f84dd21f5577
Liam Gaffney <lpgaff@gmail.com>
2024-12-05 23:43:05 +0000

    revert to testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Run elog, for testing
-#elogd -p 8080 -c /usr/local/elog/elogd.cfg
+elogd -p 8080 -c /usr/local/elog/elogd.cfg
 
 # Run elog, for real (old style)
-elogd -p 8080 -c /elog-nfs-2/elogd.cfg
+#elogd -p 8080 -c /elog-nfs-2/elogd.cfg
 
 # Run elog, from EOS
 #elogd -p 8080 -c /eos/home-i/isoelog/elog/elogd.cfg
